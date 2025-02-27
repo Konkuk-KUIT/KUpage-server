baaddb1ba59b26f1897694f076fc3fc33e49fd9c
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-02-27 23:00:02 +0900

    Initial commit

---
+++ src/main/java/com/kuit/kupage/KupageApplication.java
@@ -0,0 +1,13 @@
+package com.kuit.kupage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KupageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KupageApplication.class, args);
+	}
+
+}
