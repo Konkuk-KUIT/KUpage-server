c0cba425fc411ed20fe2ac48a7caab4631b8b58d
hamhyeongju <gudwn357@gmail.com>
2025-04-06 15:44:56 +0900

    feat: 인증 토큰 객체

---
+++ src/main/java/com/kuit/kupage/common/auth/AuthMember.java
@@ -0,0 +1,37 @@
+package com.kuit.kupage.common.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@AllArgsConstructor(access = PRIVATE)
+public class AuthMember implements UserDetails {
+
+    private final Long id;
+    private final String username;
+    private final String password;
+    private final List<GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+}
