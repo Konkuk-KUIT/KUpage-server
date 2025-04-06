ef7e25db12ac5f7ac172db3825cc26bc723e10bc
hamhyeongju <gudwn357@gmail.com>
2025-04-06 22:54:22 +0900

    feat : 회원가입 여부 검증 로직 추가

--- src/main/java/com/kuit/kupage/domain/detail/service/DetailService.java
+++ src/main/java/com/kuit/kupage/domain/detail/service/DetailService.java
@@ -28,6 +28,10 @@ public class DetailService {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberException("존재하지 않는 회원입니다."));
 
+        if (member.getDetail() == null) {
+            throw new MemberException("이미 회원가입 된 멤버입니다.");
+        }
+
         Detail savedDetail = detailRepository.save(Detail.of(signupRequest.getName(),
                 signupRequest.getStudentNumber(),
                 signupRequest.getDepartName(),
