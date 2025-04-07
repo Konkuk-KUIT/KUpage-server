63f03d3759fdc2ff1236b8a2c3bbe9d62171ec9e
hamhyeongju <gudwn357@gmail.com>
2025-04-07 21:57:08 +0900

    refactor : 로깅에 stackTrace 반영하도록 수정

--- src/main/java/com/kuit/kupage/exception/KupageException.java
+++ src/main/java/com/kuit/kupage/exception/KupageException.java
@@ -7,8 +7,25 @@ import lombok.extern.slf4j.Slf4j;
 @Getter
 public class KupageException extends RuntimeException{
 
+    private static final String APP_PACKAGE_PREFIX = "com.kuit.kupage";
+
     public KupageException(String message) {
         super(message);
-        log.error("{} - message : {}", this.getClass().getSimpleName(), message);
+        StackTraceElement[] stackTrace = getStackTrace();
+
+        StringBuilder filtered = new StringBuilder();
+
+        filtered.append(this.getClass().getSimpleName())
+                .append(" - message : ")
+                .append(message)
+                .append("\n");
+
+        for (StackTraceElement element : stackTrace) {
+            if (element.getClassName().startsWith(APP_PACKAGE_PREFIX)) {
+                filtered.append("\tat ").append(element).append("\n");
+            }
+        }
+
+        log.warn(filtered.toString());
     }
 }
