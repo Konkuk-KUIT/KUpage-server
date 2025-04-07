7b269058d2888833c65cbdd24d78320fd17ac9ba
hamhyeongju <gudwn357@gmail.com>
2025-04-07 22:16:02 +0900

    refactor : record 객체로 변경

--- src/main/java/com/kuit/kupage/domain/detail/dto/SignupRequest.java
+++ src/main/java/com/kuit/kupage/domain/detail/dto/SignupRequest.java
@@ -3,29 +3,17 @@ package com.kuit.kupage.domain.detail.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kuit.kupage.domain.detail.Grade;
 import jakarta.validation.constraints.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
-public class SignupRequest {
-    @Size(min = 2)
-    private String name;
-    @NotBlank
-    private String studentNumber;
-    @NotNull
-    private Grade grade;
-    @NotBlank
-    private String college;
-    @NotBlank
-    private String departName;
-    private String githubId;
-    @Email
-    private String email;
-    @Pattern(regexp = "010-\\d{4}-\\d{4}")
-    private String phoneNumber;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate birthday;
+public record SignupRequest(
+        @Size(min = 2) String name,
+        @NotBlank String studentNumber,
+        @NotNull Grade grade,
+        @NotBlank String college,
+        @NotBlank String departName,
+        String githubId,
+        @Email String email,
+        @Pattern(regexp = "010-\\d{4}-\\d{4}") String phoneNumber,
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthday) {
 }
