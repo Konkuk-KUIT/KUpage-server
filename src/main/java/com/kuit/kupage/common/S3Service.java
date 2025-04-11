48f41dba1ebbc4ee2be57b5d62b6b095afb90cc4
nonaninona <starcraft0529@gmail.com>
2025-04-11 20:56:07 +0900

    feat : S3 업로드 기능 추가
    - 사진 업로드 시 avif 확장자로 최적화 적용

---
+++ src/main/java/com/kuit/kupage/common/S3Service.java
@@ -0,0 +1,50 @@
+package com.kuit.kupage.common;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3Client s3Client;
+    @Value("${cloud.aws.s3.bucket-name}")
+    private String bucketName;
+    @Value("${cloud.aws.cloudfront.deploy-url}")
+    private String cloudFrontUrl;
+
+    public String uploadImage(MultipartFile file) {
+        String s3FileName = "image/" + UUID.randomUUID().toString().substring(0, 10) + ".avif";
+        return uploadToS3(file, s3FileName);
+    }
+
+    public String uploadFile(MultipartFile file) {
+        String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        String s3FileName = "file/" + UUID.randomUUID().toString().substring(0, 10) + "." + extension;
+        return uploadToS3(file, s3FileName);
+    }
+
+    private String uploadToS3(MultipartFile file, String s3FileName) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(file.getContentType());
+        objectMetadata.setContentLength(file.getSize());
+
+        try {
+            PutObjectRequest s3Object = new PutObjectRequest(bucketName, s3FileName, file.getInputStream(), objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead);
+            s3Client.putObject(s3Object);
+        } catch (Exception e) {
+//            throw new KupageException("파일 업로드 중 문제가 발생했습니다.");
+        }
+
+        return cloudFrontUrl + "/" + s3FileName;
+    }
+}
