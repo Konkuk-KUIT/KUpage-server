2426e3b8f00a09b88cb6868199222a2ff5086261
hamhyeongju <gudwn357@gmail.com>
2025-04-16 09:24:43 +0900

    feat: 인가 범위 설정 및 jwt 필터 등록

--- src/main/java/com/kuit/kupage/common/config/SecurityConfig.java
+++ src/main/java/com/kuit/kupage/common/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.kuit.kupage.common.config;
 
+import com.kuit.kupage.common.auth.JwtTokenService;
+import com.kuit.kupage.common.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,10 +10,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.kuit.kupage.common.auth.AuthRole.*;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtTokenService jwtTokenService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,9 +29,21 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().permitAll()
-                )
+                .authorizeHttpRequests(authorizeRequest -> authorizeRequest
+                        .requestMatchers("/oauth2/code/discord").permitAll())
+
+                .authorizeHttpRequests(authorizeRequest -> authorizeRequest
+                        .requestMatchers("/signup")
+                        .hasRole(GUEST.getValue()))
+
+                .authorizeHttpRequests(authorizeRequest -> authorizeRequest.
+                        requestMatchers("/")
+                        .hasRole(MEMBER.getValue()))
+
+                .authorizeHttpRequests(authorizeRequest -> authorizeRequest.anyRequest().hasRole(
+                        ADMIN.getValue()))
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class)
+
                 .build();
     }
 }
