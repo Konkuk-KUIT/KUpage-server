13f7ed4f8667504961399c2d40dd24246f195307
hamhyeongju <gudwn357@gmail.com>
2025-04-16 09:26:22 +0900

    feat: 토큰 검증 필터

---
+++ src/main/java/com/kuit/kupage/common/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,82 @@
+package com.kuit.kupage.common.jwt;
+
+import com.kuit.kupage.common.auth.AuthMember;
+import com.kuit.kupage.common.auth.AuthRole;
+import com.kuit.kupage.common.auth.JwtTokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenService jwtTokenService;
+
+    public final String BEARER = "Bearer ";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = resolveToken(request);
+
+        tokenValidateAndAuthorization(token);
+
+        filterChain.doFilter(request,response);
+    }
+
+    /**
+     * @Brief 토큰 파싱하여 Bearer 타입인지 확인하고 그 부분 잘라내서 반환
+     * @return 토큰 값(Bearer 제외)
+     */
+    private String resolveToken(HttpServletRequest request) {
+        String authorization = request.getHeader(AUTHORIZATION);
+        if (StringUtils.hasText(authorization) && authorization.startsWith(BEARER)) {
+            return authorization.substring(BEARER.length());
+        }
+
+        return null;
+    }
+
+    /**
+     * @Brief 토큰 검증하고 인가 처리
+     * @param token
+     */
+    private void tokenValidateAndAuthorization(String token) {
+
+        if (token == null) {
+            setAuthentication(createGuestAuthentication());
+            return;
+        }
+
+        if (jwtTokenService.validateToken(token)) {
+            setAuthentication(jwtTokenService.getAuthentication(token));
+        }
+
+    }
+
+    private void setAuthentication(Authentication authentication) {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private static Authentication createGuestAuthentication() {
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(AuthRole.GUEST.getRole());
+
+        //todo 이거 수정
+        return new UsernamePasswordAuthenticationToken(
+                new AuthMember(1L, authorities),
+                "", authorities);
+    }
+}
