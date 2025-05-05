9b12df66b7e6b5ba8d216a27cf60cbaf47dfaeba
hamhyeongju <gudwn357@gmail.com>
2025-05-05 19:06:11 +0900

    feat: 세부사항 추가

--- src/main/java/com/kuit/kupage/common/auth/AuthRole.java
+++ src/main/java/com/kuit/kupage/common/auth/AuthRole.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 @Getter
 public enum AuthRole {
 
-    GUEST("GUEST"),
-    MEMBER("MEMBER"),
-    ADMIN("ADMIN");
+    GUEST("GUEST"), // 디스코드 인증 후 회원가입 시점까지 사용
+    DEFAULT("DEFAULT"), // 회원가입 완료 후 role 업데이트 전까지 사용
+    MEMBER("MEMBER"), // 일반 부원
+    TUTOR("TUTOR"), // 튜터
+    ADMIN("ADMIN"); // 운영진
 
     AuthRole(String value) {
         this.value = value;
