a748257bfaafeafbc64dd6ea1b87d74795a8e395
hamhyeongju <gudwn357@gmail.com>
2025-05-06 00:38:30 +0900

    feat: role에 따른 authRole 매핑 로직 추가

--- src/main/java/com/kuit/kupage/domain/role/Role.java
+++ src/main/java/com/kuit/kupage/domain/role/Role.java
@@ -1,9 +1,13 @@
 package com.kuit.kupage.domain.role;
 
+import com.kuit.kupage.common.auth.AuthRole;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 @Getter
@@ -15,7 +19,26 @@ public class Role {
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private Batch batch;
-    private String name;
-    private String part;
+    private Batch batch; // 기수
+    @Enumerated(EnumType.STRING)
+    private Name name; // 부원, 튜터, 파트장, 운영진..
+    private String part; // 서버, 안드...
+
+    public AuthRole getAuthRole() {
+        return RoleMapper.getRole(name.name());
+    }
+
+    static class RoleMapper {
+        private static final Map<String, AuthRole> roleMapper = new HashMap<>();
+
+        static {
+            roleMapper.put("부원", AuthRole.MEMBER);
+            roleMapper.put("튜터", AuthRole.TUTOR);
+            roleMapper.put("운영진", AuthRole.MEMBER);
+        }
+
+        private static AuthRole getRole(String name) {
+            return roleMapper.get(name);
+        }
+    }
 }
