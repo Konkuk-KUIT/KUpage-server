889b5fc5487ace10a5b75d7f0d11dccaa5061084
hamhyeongju <gudwn357@gmail.com>
2025-05-08 21:56:27 +0900

    refactor: 피그마 명세에 맞게 수정

--- src/main/java/com/kuit/kupage/domain/detail/Grade.java
+++ src/main/java/com/kuit/kupage/domain/detail/Grade.java
@@ -1,5 +1,8 @@
 package com.kuit.kupage.domain.detail;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -14,8 +17,23 @@ public enum Grade {
     SIXTH_YEAR("6학년"),
     LEAVE_OF_ABSENCE("휴학"),
     EXTENDED_SEMESTER("초과학기"),
-    GRADUATED("졸업"),
-    GRADUATION_DEFERRED("졸업 유예");
+    GRADUATED("졸업생"),
+    GRADUATION_DEFERRED("졸업유예");
 
     private final String description;
+
+    @JsonCreator
+    public static Grade from(String input) throws InvalidFormatException{
+        for (Grade grade : Grade.values()) {
+            if (grade.description.equals(input)) {
+                return grade;
+            }
+        }
+        throw new InvalidFormatException(null, "지원하지 않는 학년입니다.", input, Grade.class);
+    }
+
+    @JsonValue
+    public String toJson() {
+        return description; // 응답에는 "1학년"처럼 description이 나가게 됨
+    }
 }
