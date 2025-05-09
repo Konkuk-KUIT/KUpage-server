b3202b8be1e2ceccd5310c9c4cb608c7ea940f79
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-05-09 10:58:36 +0900

    fix: CORS 설정 도메인 변경

--- src/main/java/com/kuit/kupage/common/config/SecurityConfig.java
+++ src/main/java/com/kuit/kupage/common/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5379"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
