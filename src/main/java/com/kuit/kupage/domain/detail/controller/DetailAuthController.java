635dc190ba893f2d1ebce8314585b80020e07e6e
hamhyeongju <gudwn357@gmail.com>
2025-05-10 20:43:28 +0900

    refactor: BaseResponse 사용하도록 수정

--- src/main/java/com/kuit/kupage/domain/detail/controller/DetailAuthController.java
+++ src/main/java/com/kuit/kupage/domain/detail/controller/DetailAuthController.java
@@ -2,16 +2,18 @@ package com.kuit.kupage.domain.detail.controller;
 
 import com.kuit.kupage.common.auth.AuthMember;
 import com.kuit.kupage.common.auth.AuthTokenResponse;
+import com.kuit.kupage.common.response.BaseResponse;
 import com.kuit.kupage.domain.detail.dto.SignupRequest;
 import com.kuit.kupage.domain.detail.service.DetailService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.kuit.kupage.common.response.ResponseCode.SUCCESS;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,12 +23,12 @@ public class DetailAuthController {
     private final DetailService detailService;
 
     @PostMapping("/signup")
-    public ResponseEntity<AuthTokenResponse> signup(@Valid @RequestBody SignupRequest signupRequest,
+    public BaseResponse<AuthTokenResponse> signup(@Valid @RequestBody SignupRequest signupRequest,
                                                     @AuthenticationPrincipal AuthMember authMember) {
 
         AuthTokenResponse response = detailService.signup(signupRequest, authMember.getId());
 
-        return ResponseEntity.ok(response);
+        return new BaseResponse<>(SUCCESS, response);
     }
 
 }
