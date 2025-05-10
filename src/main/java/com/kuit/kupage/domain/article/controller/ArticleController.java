b39f1d5cf83181a97aff0dc7ecdd4e04f038dc65
nonaninona <starcraft0529@gmail.com>
2025-05-10 23:19:52 +0900

    feat : 응답 형태를 BaseResponse로 수정

--- src/main/java/com/kuit/kupage/domain/article/controller/ArticleController.java
+++ src/main/java/com/kuit/kupage/domain/article/controller/ArticleController.java
@@ -1,9 +1,11 @@
 package com.kuit.kupage.domain.article.controller;
 
 import com.kuit.kupage.common.auth.AuthMember;
-import com.kuit.kupage.domain.article.service.ArticleFacade;
-import com.kuit.kupage.domain.article.dto.UploadArticleRequest;
+import com.kuit.kupage.common.response.BaseResponse;
+import com.kuit.kupage.common.response.ResponseCode;
 import com.kuit.kupage.domain.article.domain.Article;
+import com.kuit.kupage.domain.article.dto.UploadArticleRequest;
+import com.kuit.kupage.domain.article.service.ArticleFacade;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,8 +22,8 @@ public class ArticleController {
     private final ArticleFacade articleFacade;
 
     @PostMapping("/articles")
-    public Long uploadArticle(@Valid @RequestBody UploadArticleRequest request, @AuthenticationPrincipal AuthMember authMember) {
-        Article article = articleFacade.createArticle(request, authMember.getId());
-        return article.getId();
+    public BaseResponse<UploadArticleResponse> uploadArticle(@Valid @RequestBody UploadArticleRequest request, @AuthenticationPrincipal AuthMember authMember) {
+        Article article = articleFacade.createArticle(request, 1L);
+        return new BaseResponse<>(ResponseCode.SUCCESS, new UploadArticleResponse(article.getId()));
     }
 }
