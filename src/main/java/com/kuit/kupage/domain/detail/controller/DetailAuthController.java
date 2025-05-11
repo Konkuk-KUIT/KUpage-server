a46a0062ba9c4904dd3006443b2aef957e2f2974
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-05-12 00:16:09 +0900

    style : swagger 설명 추가 (#18)

--- src/main/java/com/kuit/kupage/domain/detail/controller/DetailAuthController.java
+++ src/main/java/com/kuit/kupage/domain/detail/controller/DetailAuthController.java
@@ -5,6 +5,9 @@ import com.kuit.kupage.common.auth.AuthTokenResponse;
 import com.kuit.kupage.common.response.BaseResponse;
 import com.kuit.kupage.domain.detail.dto.SignupRequest;
 import com.kuit.kupage.domain.detail.service.DetailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,13 +21,18 @@ import static com.kuit.kupage.common.response.ResponseCode.SUCCESS;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "유저 상세 정보 인증 Controller", description = "유저 상세 정보와 인증 관련 Controller 입니다.")
 public class DetailAuthController {
 
     private final DetailService detailService;
 
     @PostMapping("/signup")
-    public BaseResponse<AuthTokenResponse> signup(@Valid @RequestBody SignupRequest signupRequest,
-                                                    @AuthenticationPrincipal AuthMember authMember) {
+    @Operation(summary = "최종 회원가입 API", description = "유저의 상세 정보를 등록하고 회원가입을 마칩니다.")
+    public BaseResponse<AuthTokenResponse> signup(
+            @Valid @RequestBody SignupRequest signupRequest,
+            @Parameter(hidden = true)
+            @AuthenticationPrincipal AuthMember authMember
+    ) {
 
         AuthTokenResponse response = detailService.signup(signupRequest, authMember.getId());
 
