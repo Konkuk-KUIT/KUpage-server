c052e4530e305b84a2bcbeea633bef079ea8b134
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-10-31 13:22:21 +0900

    fix: 코드리뷰 반영

--- src/main/java/com/kuit/kupage/domain/member/repository/MemberRepository.java
+++ src/main/java/com/kuit/kupage/domain/member/repository/MemberRepository.java
@@ -2,12 +2,11 @@ package com.kuit.kupage.domain.member.repository;
 
 import com.kuit.kupage.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByDiscordId(String discordId);
