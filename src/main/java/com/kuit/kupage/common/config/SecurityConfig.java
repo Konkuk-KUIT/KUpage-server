49b13c1362bbfd7fcb928a6346ac3d1e5ca0a8a3
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-11-26 16:56:46 +0900

    chore: 프론트 도메인 CORS 설정

--- src/main/java/com/kuit/kupage/common/config/SecurityConfig.java
+++ src/main/java/com/kuit/kupage/common/config/SecurityConfig.java
@@ -78,7 +78,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://konkuk-kuit.github.io"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
